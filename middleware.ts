47b57ff570496a2265e5331aef58679ed12dcb3e
Mudit-Jxin7 <muditert34@gmail.com>
2024-08-03 21:32:25 +0530

    feat: protected routes

---
+++ middleware.ts
@@ -0,0 +1,16 @@
+import { NextResponse } from "next/server";
+import type { NextRequest } from "next/server";
+
+export async function middleware(req: NextRequest, res: NextResponse) {
+    const cookies = req.cookies.get("token");
+    if (!cookies || !cookies.value) {
+        return NextResponse.redirect(new URL("/auth/login", req.url));
+    }
+    return NextResponse.next();
+}
+
+export const config = {
+    matcher: [
+        "/",
+    ],
+};
