ec9da1c81c8bf29f2481fee804d97f4e50ed3ab8
Mudit-Jxin7 <muditert34@gmail.com>
2024-08-09 16:49:25 +0530

    feat: Add dialog for creating testimonials and previewing spaces

--- app/space/[id]/page.tsx
+++ app/space/[id]/page.tsx
@@ -16,6 +16,14 @@ import {
   CardHeader,
   CardTitle,
 } from "@/components/ui/card";
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
 
 const page = () => {
   const { id } = useParams();
@@ -37,10 +45,69 @@ const page = () => {
       <div className="flex flex-row justify-between px-40">
         <div className="flex flex-col gap-2">
           <h2 className="text-lg font-medium">INBOX</h2>
-          <p className="text-md font-normal">All</p>
-          <p className="text-md font-normal">Texts</p>
-          <p className="text-md font-normal">Liked</p>
-          <p className="text-md font-normal">Hall of fame</p>
+          <p className="text-md font-normal cursor-pointer">All</p>
+          <p className="text-md font-normal cursor-pointer">Texts</p>
+          <p className="text-md font-normal cursor-pointer">Liked</p>
+          <p className="text-md font-normal cursor-pointer">Hall of fame</p>
+          <Dialog>
+            <DialogTrigger>
+              <p className="text-md font-normal text-left cursor-pointer">
+                Preview
+              </p>
+            </DialogTrigger>
+            <DialogContent>
+              <Card>
+                <CardHeader>
+                  <CardTitle>Space Name</CardTitle>
+                  <CardDescription>Space Header Name</CardDescription>
+                </CardHeader>
+                <CardContent>
+                  <p>Space Description</p>
+                </CardContent>
+                <CardContent>
+                  <p>Questions</p>
+                  <ul className="list-disc">
+                    <li>Who are you / What you are working on?</li>
+                    <li>How has [our product / service] helped you?</li>
+                    <li>
+                      What is the best thing about [our product / service]?
+                    </li>
+                  </ul>
+                </CardContent>
+              </Card>
+            </DialogContent>
+          </Dialog>
+          <Dialog>
+            <DialogTrigger>
+              <Button
+                className="bg-purple-500 text-white py-2 rounded-md hover:bg-purple-600 transition-colors"
+                size={"sm"}
+              >
+                Create Testimonial
+              </Button>
+            </DialogTrigger>
+            <DialogContent>
+              <DialogHeader>
+                <DialogTitle>Create a Testimonial</DialogTitle>
+                <DialogDescription>Please fill the details.</DialogDescription>
+              </DialogHeader>
+              <div className="flex flex-col gap-4">
+                <Input
+                  type="text"
+                  className="border rounded-md p-2"
+                  placeholder="Enter Header Name..."
+                />
+                <Input
+                  type="text"
+                  className="border rounded-md p-2"
+                  placeholder="Enter Header Message..."
+                />
+                <Button className="bg-purple-500 text-white py-2 rounded-md hover:bg-purple-600 transition-colors">
+                  Create
+                </Button>
+              </div>
+            </DialogContent>
+          </Dialog>
         </div>
         <div className="flex flex-col gap-6 w-3/5">
           <div className="flex flex-row gap-4">
